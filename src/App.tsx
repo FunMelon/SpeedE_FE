401a36f6ca30167189dfca938adf999d686ac398
FunMelon <Fun_Melon@163.com>
2022-11-18 08:14:37 +0800

    A simple login form format.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,56 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header></Header>
+            <Wrapper></Wrapper>
+        </div>
+    );
+}
+
+// 导航栏
+function Header() {
+    return (
+        <nav>
+            我是导航栏
+        </nav>
+    )
+}
+
+// 网页主体
+function Wrapper() {
+    return (
+        <section id={"wrapper"}>
+           <Login></Login>
+        </section>
+    )
+}
+
+// 表单
+function Login() {
+    return (
+        <form className={"mainForm"}>
+            <h2 className={"login_h2"}>
+                登录
+                <a href={""}>没有账号?立即注册</a>
+            </h2>
+            <ul>
+                <li>
+                    <input className={"login_input"} type={"text"}></input>
+                </li>
+                <li>
+                    <input className={"login_input"} type={"password"}/>
+                </li>
+                <li>
+                    <label className={"login_check"}><input type={"checkbox"}/>记住密码</label>
+                    <a href={""} className={"fr"}>忘记密码?</a>
+                </li>
+            </ul>
+            <input type={"submit"} value={"登录"} className={"login_submit"}/>
+        </form>
+    )
 }
 
 export default App;

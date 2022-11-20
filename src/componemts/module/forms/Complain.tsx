ccf8ced639330b3b99055641187ffda53a92b763
saimahuidi <120583148@qq.com>
2022-11-20 22:49:34 +0800

    complain form

--- src/componemts/module/forms/Complain.tsx
+++ src/componemts/module/forms/Complain.tsx
@@ -6,10 +6,21 @@ const ComplainForm = () => {
     return (
         <form className='form_wrapper' id='complain_form'>
             <CloseButton formName='complain_form'/>
-            <p>sdbfbjk</p>
-            <p>sdbfbjk</p>
-            <p>sdbfbjk</p>
-            <p>sdbfbjk</p>
+            <div className='form_content_wrapper'>
+                <p><b>请公正填写您的申诉请求</b></p>
+            </div>
+            <div className="form_content_wrapper">
+                申诉单号：
+                <input type="text" name="" />
+            </div>
+            <div className="form_content_wrapper">
+                申诉理由：
+                <input type="text" name="" />
+            </div>
+            <div className="form_content_wrapper">
+                申诉说明：
+                <textarea  name="" />
+            </div>
         </form>
     )
 }
